87dcdf38653677ecbde620f14c79b33b9bad0412
James <jimmylaunder21@gmail.com>
2022-02-11 14:27:13 +1100

    added single message spam

--- spam.py
+++ spam.py
@@ -1,8 +1,10 @@
 # Launder
-# used for reacting to a certain message
-# started 11/02/22
+# script for spamming one message
+# started dev on 11/02/22
+
 from os import kill
 import discum
+import time
 
 # function for reading file and extracting the data each line
 def read_file(file_name):
@@ -17,20 +19,18 @@ CHANNELID = (read_file('config.txt')[1]).strip()
 
 bot = discum.Client(token=TOKEN, log=False) # discord token to login with
 print("Amount of Available Accounts: "+str(tokenNum))
-NUM_SEND = int(input("amount of reacts:")) # takes console input
+NUM_SEND = int(input("amount of msg:")) # takes console input
 if (NUM_SEND > tokenNum): # checks to see if too many reacts 
-    print("Can't react that many times.")
+    print("Can't msg that many times.")
     exit(200) #exits out
-MESSAGEID = input("messageId:")
-EMOJI = input("emoji:")
+MESSAGE = input("message:")
 count = 0
 # loop to run through and react to ceartain id
 for token in TOKENS:
-    bot.addReaction(CHANNELID,MESSAGEID,EMOJI)
     bot.switchAccount(token.strip())
+    bot.typingAction(CHANNELID)
+    time.sleep(5)
+    bot.sendMessage(CHANNELID, MESSAGE)
     count+=1
     if (count == NUM_SEND):
         break
-
-
-
