7ef98fcbd6809688555dbac0d5852f4aaaec328b
James <jimmylaunder21@gmail.com>
2022-02-15 12:18:00 +1100

    added nice start screen

--- start.py
+++ start.py
@@ -7,7 +7,15 @@ import main
 import react
 import spam
 
-
+print(" _                           _           ")
+print("| |                         | |          ")
+print("| |     __ _ _   _ _ __   __| | ___ _ __ ")
+print("| |    / _` | | | | '_ \ / _` |/ _ \ '__|")
+print("| |___| (_| | |_| | | | | (_| |  __/ |   ")
+print("|______\__,_|\__,_|_| |_|\__,_|\___|_|")
+print("For the message react press 1")
+print("For the conversation creator press 2")
+print("For the message spam press 3")
 id = int(input("id:"))
 
 if (id==1):
@@ -21,4 +29,3 @@ if (id==3):
 
 # TO-DO 
 # CAN USE THIS TO CREATE MULTITHREADING
-# CREATE A NICE START SCREEN TO DISPLAY FUNCTIONS
